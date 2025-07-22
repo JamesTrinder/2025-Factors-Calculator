4e0591e9d4ed7d6fed87871c6e00a3d050560486
JamesTrinder <trinderj4203@masseyhigh.school.nz>
2025-07-23 10:29:11 +1200

    Factors Instructions
    displays instructions like a cool guy (but only when you want)

---
+++ C_01_factors_intstructions.py
@@ -0,0 +1,15 @@
+# Write instructions
+def instructions():
+    print('''Instructions:
+    1. holy moley
+    2. long beef
+    3. time had name
+    4. etc, etc''')
+
+# Display instructions
+want_instructions = input("Press enter to see instructions "
+                          "or any key to continue ")
+if want_instructions == "":
+    instructions()
+
+# Code continues...
